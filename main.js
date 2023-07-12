7006438163109f80931719e6b5d26d7812cde141
AnusreeRoy <119887004+AnusreeRoy@users.noreply.github.com>
2023-07-13 01:28:35 +0600

    update

---
+++ main.js
@@ -0,0 +1,13 @@
+// to print
+console.log("Hello World")
+// to write a variable
+// For dynamic type
+let name = "Anusree"
+let number= 1
+// for constant type
+const pi= 3.1416
+console.log(pi)
+let a=prompt("what is ur name?"); console.log(a)
+// console.log() shows only to the user
+// alert shows the userb as well
+let b=prompt("what is ur name?"); alert(b)
